feef87f6e66c1d61938825bc1407817a482d511e
masyuraC7 <masyurafanni1@gmail.com>
2023-11-28 22:13:35 +0700

    add JanTune app icon, add design SplashScreenActivity

--- app/src/main/java/com/jantune/heartdisease/ui/view/splash/SplashScreenActivity.kt
+++ app/src/main/java/com/jantune/heartdisease/ui/view/splash/SplashScreenActivity.kt
@@ -2,18 +2,45 @@ package com.jantune.heartdisease.ui.view.splash
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 import com.jantune.heartdisease.R
 import com.jantune.heartdisease.ui.view.auth.AuthActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @SuppressLint("CustomSplashScreen")
 class SplashScreenActivity : AppCompatActivity() {
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
 
-        val intentToAuthActivity = Intent(this, AuthActivity::class.java)
-        startActivity(intentToAuthActivity)
+        setupFullscreen()
+
+        GlobalScope.launch {
+            delay(2000)
+
+            val intentToAuthActivity = Intent(this@SplashScreenActivity, AuthActivity::class.java)
+            startActivity(intentToAuthActivity)
+        }
+    }
+
+    private fun setupFullscreen() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+
+        supportActionBar?.hide()
     }
 }
